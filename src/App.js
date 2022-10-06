5931b8c35372c5de1c831032807983cde6068d7e
Casey Ocampo <caseysoflo@gmail.com>
2022-10-06 18:34:56 -0400

    Add H1 and styles to keep previous div styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import twitter from './images/twitter.svg'
-import github from './images/github.svg'
-import linkedin from './images/linkedin.svg'
-import alarm from './sounds/alarm.mp3';
+import React from "react"
+import "./App.css"
+import twitter from "./images/twitter.svg"
+import github from "./images/github.svg"
+import linkedin from "./images/linkedin.svg"
+import alarm from "./sounds/alarm.mp3"
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       workTimer: 900,
@@ -18,19 +18,19 @@ class App extends React.Component {
       break: false,
       workTime: 15,
       breakTime: 5,
-      backgroundColor: '#f9b08a',
+      backgroundColor: "#f9b08a",
     }
 
-    this.audio = new Audio(alarm);
+    this.audio = new Audio(alarm)
 
-    this.startTimer = this.startTimer.bind(this);
-    this.countDown = this.countDown.bind(this);
+    this.startTimer = this.startTimer.bind(this)
+    this.countDown = this.countDown.bind(this)
     this.breakCountDown = this.breakCountDown.bind(this)
-    this.breakTime = this.countDown.bind(this);
-    this.resetSettings = this.resetSettings.bind(this);
-    this.pauseTimer = this.pauseTimer.bind(this);
-    this.selectWorkTime = this.selectWorkTime.bind(this);
-    this.selectBreaktime = this.selectBreakTime.bind(this);
+    this.breakTime = this.countDown.bind(this)
+    this.resetSettings = this.resetSettings.bind(this)
+    this.pauseTimer = this.pauseTimer.bind(this)
+    this.selectWorkTime = this.selectWorkTime.bind(this)
+    this.selectBreaktime = this.selectBreakTime.bind(this)
   }
 
   startTimer(e) {
@@ -39,14 +39,13 @@ class App extends React.Component {
         this.setState({
           running: true,
           paused: false,
-          backgroundColor: '#f9b08a',
+          backgroundColor: "#f9b08a",
         })
-      }
-      else {
+      } else {
         this.setState({
           running: true,
           workInterval: setInterval(this.countDown, 1000),
-          backgroundColor: '#f9b08a',
+          backgroundColor: "#f9b08a",
         })
       }
     }
@@ -57,12 +56,11 @@ class App extends React.Component {
 
     if (this.state.workTimer > 0 && this.state.running) {
       this.setState({
-        workTimer: this.state.workTimer - 1
+        workTimer: this.state.workTimer - 1,
       })
-    }
-    else {
+    } else {
       if (!this.state.paused) {
-        clearInterval(this.state.workInterval);
+        clearInterval(this.state.workInterval)
         this.setState({
           break: true,
           breakInterval: setInterval(this.breakCountDown, 1000),
@@ -74,35 +72,34 @@ class App extends React.Component {
 
   breakCountDown(e) {
     this.state.breakTimer === 5 && this.audio.play()
-    
+
     if (this.state.breakTimer > 0 && this.state.running) {
       this.setState({
         breakTimer: this.state.breakTimer - 1,
-        backgroundColor: '#86f490',
+        backgroundColor: "#86f490",
       })
-    }
-    else {
+    } else {
       if (!this.state.paused) {
-        clearInterval(this.state.breakInterval);
+        clearInterval(this.state.breakInterval)
         this.setState({
           break: false,
           workInterval: setInterval(this.countDown, 1000),
           breakTimer: this.state.breakTime * 60,
-          backgroundColor: '#86f490',
+          backgroundColor: "#86f490",
         })
       }
     }
   }
 
   resetSettings(e) {
-    clearInterval(this.state.workInterval);
-    clearInterval(this.state.breakInterval);
+    clearInterval(this.state.workInterval)
+    clearInterval(this.state.breakInterval)
     this.setState({
       running: false,
       paused: false,
       workTimer: this.state.workTime * 60,
       breakTimer: this.state.breakTime * 60,
-      backgroundColor: '#f9b08a',
+      backgroundColor: "#f9b08a",
     })
   }
 
@@ -116,24 +113,24 @@ class App extends React.Component {
   selectWorkTime(event) {
     this.setState({
       workTime: event.target.value,
-      workTimer: event.target.value * 60
+      workTimer: event.target.value * 60,
     })
   }
 
   selectBreakTime(event) {
     this.setState({
       breakTime: event.target.value,
-      breakTimer: event.target.value * 60
+      breakTimer: event.target.value * 60,
     })
   }
 
   render() {
-    var settingClassName = "Control-Panel " + (this.state.running || this.state.paused ? "disableSettings" : null);
+    var settingClassName = "Control-Panel " + (this.state.running || this.state.paused ? "disableSettings" : null)
     return (
       <div className="App">
-        <header className="App-header" style={{backgroundColor: `${this.state.backgroundColor}`}}>
+        <header className="App-header" style={{ backgroundColor: `${this.state.backgroundColor}` }}>
           <div className="App-Title">
-            <div>T🍅️mate</div>
+            <h1 style={{ fontSize: "calc(60px + 2vmin)", fontWeight: "400" }}>T🍅️mate</h1>
           </div>
           <div className={settingClassName}>
             <div className="Timer-Controls">
@@ -165,18 +162,34 @@ class App extends React.Component {
               </select>
             </div>
           </div>
-          <div style={{display: !this.state.break ? 'block' : 'none'}}>
+          <div style={{ display: !this.state.break ? "block" : "none" }}>
             <div>Work Time</div>
-            <div>{Math.floor(this.state.workTimer / 60)} mins {this.state.workTimer % 60} secs</div>
+            <div>
+              {Math.floor(this.state.workTimer / 60)} mins {this.state.workTimer % 60} secs
+            </div>
           </div>
-          <div style={{display: this.state.break ? 'block' : 'none'}}>
+          <div style={{ display: this.state.break ? "block" : "none" }}>
             <div>Break Time</div>
-            <div>{Math.floor(this.state.breakTimer / 60)} mins {this.state.breakTimer % 60} secs</div>
+            <div>
+              {Math.floor(this.state.breakTimer / 60)} mins {this.state.breakTimer % 60} secs
+            </div>
           </div>
           <div className="App-Buttons">
-            <div><button className="myButton Stop" type="button" onClick={this.resetSettings} disabled={!this.state.running && !this.state.paused}>Reset</button></div>
-            <div><button className="myButton Pause" type="button" onClick={this.pauseTimer} disabled={!this.state.running}>Pause</button></div>
-            <div><button className="myButton Go" type="button" onClick={this.startTimer} disabled={this.state.running}>Start</button></div>
+            <div>
+              <button className="myButton Stop" type="button" onClick={this.resetSettings} disabled={!this.state.running && !this.state.paused}>
+                Reset
+              </button>
+            </div>
+            <div>
+              <button className="myButton Pause" type="button" onClick={this.pauseTimer} disabled={!this.state.running}>
+                Pause
+              </button>
+            </div>
+            <div>
+              <button className="myButton Go" type="button" onClick={this.startTimer} disabled={this.state.running}>
+                Start
+              </button>
+            </div>
           </div>
           <div className="Social-Buttons">
             <div>
@@ -197,8 +210,8 @@ class App extends React.Component {
           </div>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
